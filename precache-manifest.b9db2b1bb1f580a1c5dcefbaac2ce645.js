d1ac060e9a4bb8266853fc44eef7852ad6aa608a
Thomas Craig <thomascraig@users.noreply.github.com>
2020-04-24 07:45:59 -0300

    Updates

---
+++ precache-manifest.b9db2b1bb1f580a1c5dcefbaac2ce645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a5d020ea71eaba4c3079caf6cf8cd7",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "6130928476f21a2b6c45",
+    "url": "/react-calculator/static/css/main.b6e75e91.chunk.css"
+  },
+  {
+    "revision": "0b4c4cc3228f3373fe87",
+    "url": "/react-calculator/static/js/2.c8331d0d.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.c8331d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6130928476f21a2b6c45",
+    "url": "/react-calculator/static/js/main.307414a2.chunk.js"
+  },
+  {
+    "revision": "4ad8d72ddf01a440dc4b",
+    "url": "/react-calculator/static/js/runtime-main.d3b7e15a.js"
+  }
+]);
